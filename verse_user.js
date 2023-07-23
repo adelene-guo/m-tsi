a8eb37bc34bb66b7dc27b1e4d459f8031b83e597
adelene-guo <69374048+adelene-guo@users.noreply.github.com>
2023-07-23 15:52:27 -0400

    a

--- verse_user.js
+++ verse_user.js
@@ -1,2 +1,29 @@
 var number = 20;
-document.getElementById("number").innerHTML = "# of Contributers: "+number;
+var currentSong = 0;
+const songnames = [
+    "Miss the Rage (feat. PlayBoi Carti)",
+    "Moonlight Sonata",
+    "Attention"
+]
+const singers = [
+    "Trippie Redd",
+    "Ludwig van Beethoven",
+    "New Jeans"
+]
+
+document.getElementById("number").innerHTML = number;
+
+//forever:
+document.getElementById("songName").innerHTML = songnames[currentSong]+"<p style='color: #CC5500;'>"+singers[currentSong];
+//when song is over:
+currentSong+=1
+
+function mixingOn(){
+    var mixSwitch = document.getElementById("mixingOn")
+    if (mixSwitch.checked == true){
+        document.getElementById("mixingText").innerHTML = "mixing on";
+    }
+    else if (mixSwitch.checked == false){
+            document.getElementById("mixingText").innerHTML = "mixing off";
+    }
+   }
