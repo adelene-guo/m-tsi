30da123d56b8263960b348d28aef310d3fe9acd3
adelene-guo <69374048+adelene-guo@users.noreply.github.com>
2023-07-26 04:40:50 -0400

    f

--- verse_user.js
+++ verse_user.js
@@ -1,5 +1,7 @@
-var number = 20;
+var number = 0;
 var currentSong = 0;
+number++; // how to make the variable universal cross all accounts?
+
 const songnames = [
     "Miss the Rage (feat. PlayBoi Carti)",
     "Moonlight Sonata",
@@ -32,13 +34,55 @@ function mixingOn(){
 
    function addToQueue() {
        const inputField = document.getElementById("song-input");
+       const inputField1 = document.getElementById("artist-input");
        const songName = inputField.value.trim();
+       const artistName = inputField1.value.trim();
 
-       if (songName !== "") {
-           queue.push(songName);
+       if (songName !== "" && artistName !== "") {
+           queue.push(songName +" by "+artistName);
            inputField.value = "";
+           inputField1.value = "";
            renderQueue();
        }
+       /*add song to spreadsheet
+
+    try:
+        search_result = sp.search(songName, limit=1, type='track')
+        track = search_result['tracks']['items'][0]
+        track_id = track['id']
+
+        # Check for duplicates before adding
+        if track_id not in added_track_ids:
+            # Get audio features for the track
+            audio_features = sp.audio_features(track_id)[0]
+
+            # Append data to the dataframe
+            track_df.loc[track_id] = {
+                'ID': track_id,
+                'Album Cover': track['album']['images'][0]['url'] if track['album']['images'] else None,
+                'Duration (ms)': track['duration_ms'],
+                'Popularity': track['popularity'],
+                'Danceability': audio_features['danceability'],
+                'Energy': audio_features['energy'],
+                'Valence': audio_features['valence'],
+                'Key': audio_features['key'],
+                'Tempo': audio_features['tempo'],
+                'Time Signature': audio_features['time_signature']
+            }
+
+            sp.playlist_add_items(playlist_id, [track_id])
+            added_track_ids.append(track_id)
+            print("added: " + str(song))
+
+        else:
+            print(f"Duplicate song {song} skipped.")
+
+    except IndexError:
+        print(f"Couldn't find {song} on Spotify.")
+    except spotipy.exceptions.SpotifyException as e:
+        print(f"Error searching for {song}. Error: {e}")
+*/
+       
    }
 
    function removeFromQueue(index) {
